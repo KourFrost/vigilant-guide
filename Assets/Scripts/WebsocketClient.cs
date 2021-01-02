8fa033635850d900fdb0e327f2edc02df24d7030
noah card <noahcard@mail.com>
2021-01-02 15:12:11 -0700

    Added Mirror and websocket client beginnings

---
+++ Assets/Scripts/WebsocketClient.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using WebSocketSharp;
+public class WebsocketClient : MonoBehaviour
+{
+    WebSocket ws;
+    public GameObject myPrefab;
+    private void Start()
+    {
+        ws = new WebSocket("ws://localhost:8080");
+        ws.Connect();
+        ws.OnMessage += (sender, e) =>
+        {
+            ServerMessage serverMessage = JsonUtility.FromJson<ServerMessage>(e.Data);
+
+            // ServerMessage serverMessage = JsonUtility.FromJson<ServerMessage>(e.Data);
+            // switch (serverMessage.message) {
+            //     case ServerMessagePlayers.message:
+            //         WebsocketPlayerModel[] playerModels = (WebsocketPlayerModel[])serverMessage.data;
+                    Debug.Log("List of Players"+serverMessage);
+            //     break;
+            // }
+            // Debug.Log("Message Received from "+((WebSocket)sender).Url+", Data : "+e.Data);
+            foreach(var playerModel in serverMessage.data)
+            {
+                Debug.Log(playerModel);
+                Instantiate(myPrefab, new Vector3(playerModel.position.x, playerModel.position.y, 0), Quaternion.identity);
+            }
+        };
+    }
+    private void Update()
+    {
+        if(ws == null)
+        {
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ws.Send("get players");
+        }  
+    }
+}
---
+++ Assets/Scripts/WebsocketPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class ServerMessage
+{
+    public string message;
+    public WebsocketPlayerModel[] data;
+}
+
+[Serializable]
+public class ClientGetPlayersMessage
+{
+    public string MESSAGE = "get players";
+}
+[Serializable]
+public class ClientAddPlayerMessage
+{
+    public string MESSAGE = "add players";
+}
+
+[Serializable]
+public class ServerMessagePlayers
+{
+    public const string message = "players";
+}
+
+[Serializable]
+public class WebsocketPlayerModel
+{
+    public int playerId;
+    public Vector2 position;
+}
